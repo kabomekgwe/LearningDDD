1c7f0991197dfb7b7866ce0cdbee657bc429cf5a
kabo <kmekgwe@jhb.co.za>
2019-12-05 10:16:14 +0200

    added database bootstrap

---
+++ src/Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ src/Persistence/EFBoostrap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class EFBoostrap
+    {
+        public static void ConfigureServices_InfrastructureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            string migrationsAssembly = typeof(DataContext).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString, sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(migrationsAssembly);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                    }
+                ), ServiceLifetime.Transient);
+
+        }
+
+        public static void Configure_InfrastructureDatabase(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                DataContext context = scope.ServiceProvider.GetService<DataContext>();
+                context.Database.Migrate();
+            }
+        }
+    }
+}
